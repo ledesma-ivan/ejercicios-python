73315d0ec443849dc94a1a4732fc5866b7297cc1
Ivan Ledesma <ivanarielledesma@gmail.com>
2025-03-16 16:11:30 -0300

    Ejercicio 8

---
+++ ejercicio8.py
@@ -0,0 +1,9 @@
+# Realizar un programa que imprima en pantalla los números del 0 al 100
+
+for x in range(101):
+    print(x)
+
+# Realizar un programa que imprima en pantalla los números del 20 al 30.
+
+for x in range(20,31):
+    print(x)
