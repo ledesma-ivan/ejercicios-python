360e7021f36c2df476de66272988a405da914ee7
Ivan Ledesma <ivanarielledesma@gmail.com>
2025-03-19 15:56:50 -0300

    Avance de ejercicio 8 :D

--- ejercicio8.py
+++ ejercicio8.py
@@ -3,15 +3,17 @@ for x in range(101):
     print(x)
 
 # Realizar un programa que imprima en pantalla los números del 20 al 30.
-for x in range(20,31):
+for x in range(20, 31):
     print(x)
 
 
 # Imprimir todos los números impares que hay entre 1 y 100.=
-for x in range(1,100,2):
+for x in range(1, 100, 2):
     print(x)
 
-# Desarrollar un programa que permita la carga de 10 valores por teclado y nos muestre posteriormente la suma de los valores ingresados y su promedio.
+# Desarrollar un programa que permita la carga de 10 valores por teclado y
+# nos muestre posteriormente la suma de los valores ingresados y su
+# promedio.
 valores = []
 for x in range(0, 10):
     n = int(input("Ingrese un valor: "))
@@ -19,9 +21,11 @@ for x in range(0, 10):
 
 suma = sum(valores)
 promedio = suma / 10
-print(f"La suma de los valores ingresados es: {suma}, y su promedio es {promedio}")
+print(
+    f"La suma de los valores ingresados es: {suma}, y su promedio es {promedio}")
 
-# Escribir un programa que solicite por teclado 10 notas de alumnos y nos informe cuántos tienen notas mayores o iguales a 7 y cuántos menores.
+# Escribir un programa que solicite por teclado 10 notas de alumnos y nos
+# informe cuántos tienen notas mayores o iguales a 7 y cuántos menores.
 notas = []
 
 mayores_o_igual_7 = []
@@ -36,11 +40,12 @@ for x in range(0, 10):
 
 cantidad_pro = len(mayores_o_igual_7)
 cantidad_no_pro = len(menores_7)
-print(f"La cantidad de alumnos con notas mayores o iguales a 7 es: {cantidad_pro} y la cantidad de alumnos con notas menores a 7 es: {cantidad_no_pro}")
+print(
+    f"La cantidad de alumnos con notas mayores o iguales a 7 es: {cantidad_pro} y la cantidad de alumnos con notas menores a 7 es: {cantidad_no_pro}")
 
 
 # Escribir un programa que lea 10 números enteros y luego muestre cuántos valores ingresados fueron múltiplos de 3 y cuántos de 5. Debemos tener en cuenta que hay números que son múltiplos de 3 y de 5 a la vez.
-# Para que sea multiplo el residuo debe ser 0 
+# Para que sea multiplo el residuo debe ser 0
 multiplo_3 = []
 multiplo_de_5 = []
 multiplo_3_5 = []
@@ -69,7 +74,8 @@ Este tipo de problemas también se puede resolver empleando la estructura repeti
 Dicha variable se carga antes de entrar a la estructura repetitiva for.
 """
 
-# En este caso no pide usar listas, si bien es una opcion viable habra que analizar el costo computacional.
+# En este caso no pide usar listas, si bien es una opcion viable habra que
+# analizar el costo computacional.
 
 
 cantidad = int(input("Ingrese la cantidad de valores a ingresar: "))
@@ -79,7 +85,7 @@ valores_igual_mayor_mil = 0
 for cantidad in range(0, cantidad):
     n = int(input("Ingrese un valor: "))
     if n >= 1000:
-        cantidad+=1
+        cantidad += 1
 
 print('La cantidad total de numeros ingresados que son mayor o igual a mil es', cantidad)
 
@@ -93,7 +99,7 @@ cantidad = int(input("Ingrese la cantidad de triangulos que tenes"))
 superfie_mayor_12 = 0
 
 for x in range(0, cantidad):
-    base = int(input("Ingrese la base del triangulo: " ))
+    base = int(input("Ingrese la base del triangulo: "))
     altura = int(input("Ingrese la altura"))
     superficie = base * altura / 2
     print('La base es', base)
@@ -101,10 +107,11 @@ for x in range(0, cantidad):
     print('La superfie es', superficie)
 
     if superficie > 12:
-        superfie_mayor_12 +=1
+        superfie_mayor_12 += 1
 print('La la cantidad de triangules cuya superficies es mayor a 12: ', superficie)
 
-# Desarrollar un programa que solicite la carga de 10 números e imprima la suma de los últimos 5 valores ingresados.
+# Desarrollar un programa que solicite la carga de 10 números e imprima la
+# suma de los últimos 5 valores ingresados.
 ultimos_5 = []
 
 for n in range(0, 10):
@@ -115,7 +122,8 @@ for n in range(0, 10):
 suma_ultimos_5 = sum(ultimos_5)
 print("La suma de los ultimos 5 valores ingresados es: ", suma_ultimos_5)
 
-# Desarrollar un programa que muestre la tabla de multiplicar del 5 (del 5 al 50)
+# Desarrollar un programa que muestre la tabla de multiplicar del 5 (del 5
+# al 50)
 
 print("Bienvenido a la tabla de multiplicar")
 for x in range(11):
@@ -124,7 +132,8 @@ for x in range(11):
     print(resultado)
 
 # Confeccionar un programa que permita ingresar un valor del 1 al 10 y nos muestre la tabla de multiplicar del mismo (los primeros 12 términos)
-# Ejemplo: Si ingreso 3 deberá aparecer en pantalla los valores 3, 6, 9, hasta el 36.
+# Ejemplo: Si ingreso 3 deberá aparecer en pantalla los valores 3, 6, 9,
+# hasta el 36.
 
 n = int(input("Ingrese el valor de la tabla a multiplicar: "))
 
@@ -141,25 +150,26 @@ b) Cantidad de triángulos de cada tipo.
 
 n = int(input("Ingrese cuantos triangulos tenes: "))
 equilátero = 0
-isosceles = 0 
+isosceles = 0
 escaleno = 0
 
-for n in range(0, n+1):
+for n in range(0, n + 1):
     lado_1 = int(input("Ingrese el lado 1: "))
     lado_2 = int(input("Ingrese el lado 2: "))
     lado_3 = int(input("Ingrese el lado 3: "))
 
     if lado_1 == lado_2 and lado_1 == lado_3:
         print("Los tres lados son iguales")
-        equilátero+=1
+        equilátero += 1
     elif lado_1 == lado_2 or lado_1 == lado_3:
         print("El triangulo es isosceles")
-        isosceles+=1
+        isosceles += 1
     else:
         print("Ningun lado es igual")
-        escaleno+=1
- 
-print(f"La cantidad de triagulos que son equilateros, {equilátero}, y isosceles {isosceles}, y lo que son escaleno {escaleno}")
+        escaleno += 1
+
+print(
+    f"La cantidad de triagulos que son equilateros, {equilátero}, y isosceles {isosceles}, y lo que son escaleno {escaleno}")
 
 """
 Escribir un programa que pida ingresar coordenadas (x,y) que representan puntos en el plano.
@@ -181,9 +191,26 @@ numeros_pares = []
 
 for x in range(10):
     n = input("Ingrese un valor: ")
-    
+    if n > 0:
+        valores_positivos += 1
+        if n % 2 == 0:
+            numeros_pares.append(n)
+        elif n % 15 == 0:
+            multiplo_15 += 1
 
+    elif n < 0:
+        valores_negativos += 1
+        if n % 2 == 0:
+            numeros_pares.append(n)
+        elif n % 15 == 0:
+            multiplo_15 += 1
 
+total_numeros_pares = sum(numeros_pares)
+
+print("La cantidad de numeros positivos es: ", valores_positivos)
+print("La cantidad de numeros negativos es: ", valores_negativos)
+print("La cantidad de numeros multiplos de 15 es: ", multiplos_15)
+print("La suma total de los numeros pares es: ", total_numeros_pares)
 
 """
 Se cuenta con la siguiente información:
@@ -195,4 +222,3 @@ a) Obtener el promedio de las edades de cada turno (tres promedios)
 b) Imprimir dichos promedios (promedio de cada turno)
 c) Mostrar por pantalla un mensaje que indique cual de los tres turnos tiene un promedio de edades mayor.
 """
-
