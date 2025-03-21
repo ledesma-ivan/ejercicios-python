6fbf13e77cc091f07c49ba97bd249e3400742f7a
Ivan Ledesma <ivanarielledesma@gmail.com>
2025-03-20 22:13:44 -0300

    TO-DO

---
+++ ejercicios10.py
@@ -0,0 +1,13 @@
+# Realizar la carga por teclado del nombre, edad y altura de dos personas. Mostrar por pantalla el nombre de la persona con mayor altura.
+
+# Realizar la carga de dos nombres por teclado. Mostrar cual de los dos es mayor alfabéticamente o si son iguales.
+
+"""
+Realizar la carga de enteros por teclado. Preguntar después que ingresa el valor si desea cargar otro valor debiendo el operador ingresar la cadena 'si' o 'no' por teclado.
+Mostrar la suma de los valores ingresados.
+"""
+
+"""
+Realizar la carga de dos nombres de personas distintos. Mostrar por pantalla luego ordenados en forma alfabética.
+"""
+
