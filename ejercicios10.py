060d7674215fc78f6e8393401b44d136fe23786e
Ivan Ledesma <ivanarielledesma@gmail.com>
2025-03-21 23:42:00 -0300

    zzzz

--- ejercicios10.py
+++ ejercicios10.py
@@ -6,8 +6,15 @@
 Realizar la carga de enteros por teclado. Preguntar después que ingresa el valor si desea cargar otro valor debiendo el operador ingresar la cadena 'si' o 'no' por teclado.
 Mostrar la suma de los valores ingresados.
 """
-
+while True:
+    n = int(input("Ingrese un numero?"))
+    letra = input("Desea realizar otra carga: ?")
+    if letra == "si":
+        continue
+    elif letra == "no":
+        break
 """
 Realizar la carga de dos nombres de personas distintos. Mostrar por pantalla luego ordenados en forma alfabética.
 """
+
 
